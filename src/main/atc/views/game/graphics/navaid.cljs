5289b1227ba19d7f3ce0da37585c87f9c7141ca4
Daniel Leong <me@dhleong.net>
2022-08-20 21:23:28 -0400

    Tweak navaid rendering so aircraft stand out better

--- src/main/atc/views/game/graphics/navaid.cljs
+++ src/main/atc/views/game/graphics/navaid.cljs
@@ -3,11 +3,12 @@
     ["@inlet/react-pixi" :as px]
     ["pixi.js" :refer [TextStyle]]))
 
-(def label-style (TextStyle. #js {:fill "#00ff00" ; TODO
-                                  :fontSize 12}))
+(def label-style (TextStyle. #js {:fill "#f4f7ff"
+                                  :fontSize 11}))
 
 (defn entity [{:keys [id]}]
   [:> px/Text {:anchor 0.5
+               :alpha 0.4
                :x 0
                :y 0
                :style label-style
