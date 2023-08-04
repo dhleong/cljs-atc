0acc4a13d9f6be0c313c9d7b00e4ade876a8427e
Daniel Leong <me@dhleong.net>
2023-08-04 10:49:40 -0400

    When "arriving" at a :direct fix, clear the :direct instruction
    We should then either maintain the current heading or proceed on course,
    if commanded to do so

--- src/main/atc/engine/aircraft/commands.cljs
+++ src/main/atc/engine/aircraft/commands.cljs
@@ -47,11 +47,22 @@
 
 ; ======= Direct-to-point nav =============================
 
+; This is the distance in meters squared at which an aircraft is considered
+; to have "arrived" at its :direct target. We include quite a bit of slop
+; to avoid excess steering, since you should maintain heading if you pass
+; the point and don't have any other direction
+(def ^:private over-coordinate-distance-m2 (pow 6500 2.))
+
 (defn- apply-direct [aircraft commanded-to dt]
   ; NOTE: This is temporary; the real logic should account for resuming course,
   ; intercept heading, crossing altitude, etc.
-  (let [bearing-to-destination (bearing-to (:position aircraft) commanded-to)]
-    (apply-steering aircraft bearing-to-destination dt)))
+  (let [distance2 (distance-to-squared (:position aircraft) commanded-to)]
+    (if (< distance2 over-coordinate-distance-m2)
+      ; We're "at" the destination; we can stop heading towards it now
+      (update aircraft :commands dissoc :direct)
+
+      (let [bearing-to-destination (bearing-to (:position aircraft) commanded-to)]
+        (apply-steering aircraft bearing-to-destination dt)))))
 
 
 ; ======= Altitude ========================================
