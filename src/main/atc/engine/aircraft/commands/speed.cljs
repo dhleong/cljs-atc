f40354e8eedd21d4ccf85c32b51e65071b79d6bf
Daniel Leong <me@dhleong.net>
2023-09-18 17:28:03 -0400

    Attempt to automatically respect the "speed limit"

--- src/main/atc/engine/aircraft/commands/speed.cljs
+++ src/main/atc/engine/aircraft/commands/speed.cljs
@@ -1,14 +1,23 @@
 (ns atc.engine.aircraft.commands.speed
   (:require
+   [atc.config :as config]
+   [atc.data.units :refer [ft->m]]
    [atc.engine.aircraft.commands.helpers :refer [apply-rate]]))
 
-(defn apply-target-speed [{from :speed :as aircraft} commanded-speed dt]
-  (if (= from commanded-speed)
-    aircraft
+(def ^:private speed-limit-altitude-m (ft->m 10000))
 
-    (apply-rate
+(defn apply-target-speed [{from :speed :as aircraft} raw-commanded-speed dt]
+  (let [altitude (get-in aircraft [:position :z])
+        commanded-speed (if (> altitude speed-limit-altitude-m)
+                          raw-commanded-speed
+                          (min config/speed-limit-under-10k-kts
+                               raw-commanded-speed))]
+    (if (= from commanded-speed)
       aircraft
-      :target-speed [:speed]
-      {-1 :deceleration
-       1 :acceleration}
-      from commanded-speed dt)))
+
+      (apply-rate
+        aircraft
+        :target-speed [:speed]
+        {-1 :deceleration
+         1 :acceleration}
+        from commanded-speed dt))))
